7a5e1ae6d8a0f7052e08a749728bbb62c9ef6117
Iliyan Ramov <iramov91@gmail.com>
2015-04-30 23:16:24 +0300

    Added WorkoutView
    Added Workout View with listed muscle groups

---
+++ Fmi.OpenMinds.FitChallenge/Fmi.OpenMinds.FitChallenge.Web/Controllers/WorkoutController.cs
@@ -0,0 +1,21 @@
+﻿using Fmi.OpenMinds.FitChallenge.Data;
+using System.Web.Mvc;
+
+namespace Fmi.OpenMinds.FitChallenge.Web.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        private IFitChallengeDbContext context;
+
+        public WorkoutController(IFitChallengeDbContext context) 
+        {
+            this.context = context;        
+        }
+        // GET: Workout
+        public ActionResult Index()
+        {
+            
+            return View(context);
+        }
+    }
+}

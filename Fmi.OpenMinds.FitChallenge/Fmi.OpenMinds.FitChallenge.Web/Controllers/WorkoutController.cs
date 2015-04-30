e43debdde392eedab5032bc4ff9edef1df9c8d46
Iliyan Ramov <iramov91@gmail.com>
2015-05-01 00:19:38 +0300

    Added CreateView for Workout

--- Fmi.OpenMinds.FitChallenge/Fmi.OpenMinds.FitChallenge.Web/Controllers/WorkoutController.cs
+++ Fmi.OpenMinds.FitChallenge/Fmi.OpenMinds.FitChallenge.Web/Controllers/WorkoutController.cs
@@ -1,4 +1,5 @@
 ﻿using Fmi.OpenMinds.FitChallenge.Data;
+using Fmi.OpenMinds.FitChallenge.Models;
 using System.Web.Mvc;
 
 namespace Fmi.OpenMinds.FitChallenge.Web.Controllers
@@ -17,5 +18,25 @@ namespace Fmi.OpenMinds.FitChallenge.Web.Controllers
             
             return View(context);
         }
+
+        public ActionResult Create()
+        {
+            var workout = new Workout();
+            return View(workout);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(workout);
+            }
+
+            context.Workouts.Add(workout);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }

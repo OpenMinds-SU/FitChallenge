c70323f0febb5761ab1a52b2137b474f694fc29a
Iliyan Ramov <iramov91@gmail.com>
2015-05-08 17:09:39 +0300

    Added Views for Exercise

--- Fmi.OpenMinds.FitChallenge/Fmi.OpenMinds.FitChallenge.Web/Controllers/ExerciseController.cs
+++ Fmi.OpenMinds.FitChallenge/Fmi.OpenMinds.FitChallenge.Web/Controllers/ExerciseController.cs
@@ -19,5 +19,66 @@ namespace Fmi.OpenMinds.FitChallenge.Web.Controllers
             return View(context.Exercises);
         }
 
+        // GET : Exercise/Create
+        public ActionResult Create()
+        {
+            var exercise = new Exercise();
+            return View(exercise);
+        }
+
+        // POST : Exercise/Create
+        [HttpPost]
+        public ActionResult Create( Exercise exercise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(exercise);
+            }
+
+            context.Exercises.Add(exercise);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET : Exercise/Edit
+        public ActionResult Edit(int id)
+        {
+            var exerciseEdit = context.Exercises.Find(id);
+            return View("Edit", exerciseEdit);
+        }
+
+        // POST : Exercise/Edit
+        [HttpPost]
+        public ActionResult Edit(Exercise exercise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(exercise);
+            }
+
+            var oldExercise = context.Exercises.Find(exercise.Id);
+            oldExercise.Name = exercise.Name;
+            oldExercise.Url = exercise.Url;
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET : Exercise/Delete
+        public ActionResult Delete(int id)
+        {
+            return View(id);
+        }
+
+        // POST : Exercise/Delete
+        [HttpPost]
+        public ActionResult Delete(Exercise exercise)
+        {
+            var exerciseToDelete = context.Exercises.Find(exercise.Id);
+            context.Exercises.Remove(exerciseToDelete);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
